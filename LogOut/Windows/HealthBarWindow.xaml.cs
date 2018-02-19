e9801941da4382940660cc89cc353b6659405387
siegrest <179900@ttu.ee>
2018-02-19 21:16:25 +0200

    Add new healthbar

--- LogOut/Windows/HealthBarWindow.xaml.cs
+++ LogOut/Windows/HealthBarWindow.xaml.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace LogOut {
@@ -11,6 +19,7 @@ namespace LogOut {
     public partial class HealthBarWindow : Window {
         public HealthBarWindow() {
             InitializeComponent();
+            Height = 30;
         }
 
         /// <summary>
@@ -21,15 +30,7 @@ namespace LogOut {
             if (percentage < 0) percentage = 0;
             else if (percentage > 100) percentage = 100;
 
-            Rectangle.Width = Width * percentage / 100;
-            Label.Content = Math.Round(percentage) + "%";
-        }
-
-        /// <summary>
-        /// Not in use atm. Will be used to notify whether or not program is working
-        /// </summary>
-        public void SetColor(Color color) {
-            Rectangle.Fill = new SolidColorBrush(color);
+            ProgressBar_Main.Value = percentage;
         }
     }
 }
